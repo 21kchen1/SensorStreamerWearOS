bc0eb807f9dcbe0f43a11e86267dcca6024bcdd3
An <12373718+CKgiteehub@user.noreply.gitee.com>
2024-11-07 22:57:38 +0800

    用于检查字符串转数字

--- app/src/main/java/com/SensorStreamer/Utils/TypeTranDeter.java
+++ app/src/main/java/com/SensorStreamer/Utils/TypeTranDeter.java
@@ -1,5 +1,7 @@
 package com.SensorStreamer.Utils;
 
+import android.util.Log;
+
 /**
  * 判断类型转换是否成功的工具函数
  * @author chen
@@ -10,9 +12,10 @@ public class TypeTranDeter {
     public static boolean isStr2Num(String str) {
         try {
             Integer.parseInt(str);
-            return true;
         } catch (NumberFormatException e) {
+            Log.e("isStr2Num", "NumberFormatException", e);
             return false;
         }
+        return true;
     }
 }
